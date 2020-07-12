40332b95e5073f414d5bd49aad3a2c69a0e04d91
fundev <grace.devolop@gmail.com>
2020-07-12 08:07:27 +0200

    shopping list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavBar from './components/AppNavBar';
+import ShppingList from './components/ShppingList';
+import './App.css';
+
 
 function App() {
   return (
     <div>
      <AppNavBar/>
+     <ShppingList/>
     </div>
   );
 }
