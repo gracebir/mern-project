cc292c74314a49190ecfe3a2dac0002eac1c2406
fundev <grace.devolop@gmail.com>
2020-07-13 14:48:20 +0200

    scripting the build

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const items = require('./routes/api/items')
+const items = require('./routes/api/items');
+const path = require('path');
+
 
 app.use(bodyParser.json());
 
 app.use('/api/items',items);
+
+
+// Serve static assets if we're in production
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
